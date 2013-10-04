c988c2fcce0809bfb111fa6a724773300686e0bf
Siegfried Ehret <siegfried@ehret.me>
2013-10-04 21:50:33 +0200

    Experiment 4 - 3D shapes

--- js/experiment04.js
+++ js/experiment04.js
@@ -32,19 +32,27 @@ window.onload = function(){
 function drawStuff(engine) {
   //Creation of the scene
   var scene = new BABYLON.Scene(engine);
-  //scene.clearColor = new BABYLON.Color3(0, 0, 0);
+  scene.clearColor = new BABYLON.Color3(0, 0, 0);
 
   var camera = new BABYLON.FreeCamera("Camera", new BABYLON.Vector3(0, 0, -10), scene);
-  var light0 = new BABYLON.PointLight("Omni0", new BABYLON.Vector3(0, 100, 100), scene);
+  var light0 = new BABYLON.DirectionalLight("Dir0", new BABYLON.Vector3(0, -1, 0), scene);
 
-
-  //var pyramid = drawPyramid(scene);
-  //pyramid.rotation = new BABYLON.Vector3(0, -Math.PI/4, 0);
+  var pyramid = drawPyramid(scene);
   var cube = drawCube(scene);
-  //cube.rotation = new BABYLON.Vector3(Math.PI/4, 0, 0);
 
-  //pyramid.position.x = -1.5;
-  cube.position.x = 1.5;
+  var degree = 0;
+
+  setInterval(function() {
+    var value = Math.PI * degree++/180;
+    pyramid.rotation = new BABYLON.Vector3(0, value, 0);
+    cube.rotation = new BABYLON.Vector3(-Math.PI/12, -value, 0);
+    if (degree === 360) {
+      degree = 0;
+    }
+  }, 10);
+
+  pyramid.position.x = -2;
+  cube.position.x = 2;
 
   return scene;
 }
@@ -53,20 +61,31 @@ function drawPyramid(scene) {
   var pyramid = new BABYLON.Mesh('pyramid', scene);
 
   var positions = [
-    // front face
-    0, 1, 0,
-    -1, 0, -1,
-    1, 0, -1,
+    // Front face
+    0,  1,  0,
+    -1, -1,  1,
+    1, -1,  1,
 
-    // right face
-    0, 1, 0,
-    1, 0, -1,
+    // Right face
+    0,  1,  0,
+    1, -1,  1,
     1, -1, -1,
 
-    // back face
-    0, 1, 0,
+    // Back face
+    0,  1,  0,
     1, -1, -1,
-    -1, -1, -1
+    -1, -1, -1,
+
+    // Left face
+    0,  1,  0,
+    -1, -1, -1,
+    -1, -1,  1,
+
+    // Bottom face
+    -1, -1, -1,
+    1, -1, -1,
+    1, -1, 1,
+    -1, -1, 1
   ];
 
   var normals = [
@@ -80,6 +99,15 @@ function drawPyramid(scene) {
 
     1, 1, 1,
     1, 1, 1,
+    1, 1, 1,
+
+    1, 1, 1,
+    1, 1, 1,
+    1, 1, 1,
+
+    1, 1, 1,
+    1, 1, 1,
+    1, 1, 1,
     1, 1, 1
   ];
 
@@ -89,26 +117,41 @@ function drawPyramid(scene) {
     0, 0, 1,
 
     1, 0, 0,
+    0, 0, 1,
+    0, 1, 0,
+
+    1, 0, 0,
     0, 1, 0,
     0, 0, 1,
 
     1, 0, 0,
+    0, 0, 1,
     0, 1, 0,
-    0, 0, 1
+
+    0, 0, 1,
+    0, 1, 0,
+    0, 0, 1,
+    0, 1, 0
   ];
 
   var indices = [];
-  indices.push(0);
-  indices.push(1);
-  indices.push(2);
 
-  indices.push(3);
-  indices.push(4);
-  indices.push(5);
+  var i = 0;
+  while (i < 12) {
+    indices.push(i+0);
+    indices.push(i+1);
+    indices.push(i+2);
 
-  indices.push(6);
-  indices.push(7);
-  indices.push(8);
+    i = i+3;
+  }
+
+  indices.push(12);
+  indices.push(13);
+  indices.push(14);
+
+  indices.push(12);
+  indices.push(14);
+  indices.push(15);
 
   pyramid.setVerticesData(positions, BABYLON.VertexBuffer.PositionKind);
   pyramid.setVerticesData(normals, BABYLON.VertexBuffer.NormalKind);
@@ -122,44 +165,79 @@ function drawCube(scene) {
   var cube = new BABYLON.Mesh('cube', scene);
 
   var positions = [
+    // Front face
+    -1, -1, -1,
+    1, -1, -1,
+    1, 1, -1,
+    -1, 1, -1,
 
-    // front face
-    -1, -1, 0,
-    1, -1, 0,
-    1, 1, 0,
-    -1, 1, 0
-    /*
-     // right face
-     1, 1, -1,
-     1, 1, 1,
-     1, -1, 1,
-     1, -1, -1,
+    // Right face
+    1, -1, 1,
+    1, 1, 1,
+    1, 1, -1,
+    1, -1, -1,
 
-     //back face
-     1, 1, 1
-     -1, 1, 1,
-     -1, -1, 1,
-     1, -1, 1,
+    // Back face
+    -1,  1,  1,
+    1,  1,  1,
+    1, -1,  1,
+    -1, -1,  1,
 
-     // left face
-     -1, 1, -1,
-     -1, 1, 1,
-     -1, -1, 1,
-     -1, -1, -1  */
+    // Left face
+    -1, 1, 1,
+    -1, -1, 1,
+    -1, -1, -1,
+    -1, 1, -1,
+
+    // Top face
+    1, 1, -1,
+    1, 1, 1,
+    -1, 1, 1,
+    -1, 1, -1,
+
+    // Bottom face
+    -1, -1, 1,
+    1, -1, 1,
+    1, -1, -1,
+    -1, -1, -1
   ];
 
   var normals = [
-    0, 0, 1/*,
-     1, 0, 0,
-     0, 0, 1,
-     -1, 0, 0 */
-    /*,
-     0, 1, 0,
-     0, -1, 0*/
+    1, 1, 1,
+    1, 1, 1,
+    1, 1, 1,
+    1, 1, 1,
+
+    1, 1, 1,
+    1, 1, 1,
+    1, 1, 1,
+    1, 1, 1,
+
+    1, 1, 1,
+    1, 1, 1,
+    1, 1, 1,
+    1, 1, 1,
+
+    1, 1, 1,
+    1, 1, 1,
+    1, 1, 1,
+    1, 1, 1,
+
+    1, 1, 1,
+    1, 1, 1,
+    1, 1, 1,
+    1, 1, 1,
+
+    1, 1, 1,
+    1, 1, 1,
+    1, 1, 1,
+    1, 1, 1
   ];
 
   var indices = [];
-  for (var i = 0; i <= 0; i++) {
+
+  var i = 0;
+  while (i < 24) {
     indices.push(i+0);
     indices.push(i+1);
     indices.push(i+2);
@@ -167,10 +245,45 @@ function drawCube(scene) {
     indices.push(i+0);
     indices.push(i+2);
     indices.push(i+3);
+
+    i = i+4;
   }
+
+  var colors = [
+    1, 0, 0,
+    1, 0, 0,
+    1, 0, 0,
+    1, 0, 0,
+
+    0, 1, 0,
+    0, 1, 0,
+    0, 1, 0,
+    0, 1, 0,
+
+    0, 0, 1,
+    0, 0, 1,
+    0, 0, 1,
+    0, 0, 1,
+
+    1, 1, 0,
+    1, 1, 0,
+    1, 1, 0,
+    1, 1, 0,
+
+    1, 1, 1,
+    1, 1, 1,
+    1, 1, 1,
+    1, 1, 1,
+
+    1, .2, .6,
+    1, .2, .6,
+    1, .2, .6,
+    1, .2, .6
+  ];
 
   cube.setVerticesData(positions, BABYLON.VertexBuffer.PositionKind);
   cube.setVerticesData(normals, BABYLON.VertexBuffer.NormalKind);
+  cube.setVerticesData(colors, BABYLON.VertexBuffer.ColorKind);
   cube.setIndices(indices);
 
   return cube;
